8d706db8e970340217503966771f2b45c16e68ca
Kipngetich222 <123062517+Kipngetich222@users.noreply.github.com>
2024-11-29 06:10:18 +0300

    python literals

--- Basics/variables.py
+++ Basics/variables.py
@@ -1,8 +1,25 @@
 #extracting values from lists, tuples etc into variable
 fruits = ["apple", "banana", "cherry"]
 x, y, z = fruits
-print(x)
-print(y)
-print(z)
+# print(x)
+# print(y)
+# print(z)
 
 
+#List Type Literal
+L1 = [1, "Victor", 75.90, True]
+#print(L1, type(L1))
+
+#TUple Type Literal
+T1 = (1, "Victor", 75.90, True)
+#print(T1, type(T1))
+
+#Dictionary literal
+capitals={"USA":"New York", "France":"Paris", "Japan":"Tokyo",
+"India":"New Delhi"}
+numbers={1:"one", 2:"Two", 3:"three",4:"four"}
+points={"p1":(10,10), "p2":(20,20)}
+
+print (capitals, type(capitals))
+print (numbers, type(numbers))
+print (points, type(points))
